70bc5c4e7e33542660563134d1382fd1716b7443
LUCIA <cbechur0606@m.educaand.es>
2023-02-17 11:09:46 +0100

    day 4 done

--- day_4/Strings.py
+++ day_4/Strings.py
@@ -21,3 +21,32 @@ print(len("coding_for_all")) !=print(len("coding"))
 
 company2="coding_for_all"
 print(company2.replace('coding','python'))
+
+company3 = "python for all"
+print(company3.replace("python","everyone"))
+print((company2.replace("coding", "python")).replace("python","everyone"))
+
+
+print(company.split())
+
+print("Facebook , Google, Microsoft, Apple, IBM, Oracle, Amazon".split(','))
+
+print(company[0])
+print(len(company) -1)
+print(company[10])
+
+palabras = "Python For Everyone".split()
+print(palabras[0][0]+ palabras[1][0] + palabras [2][0])
+
+palabras2 = "Coding For All".split()
+print(palabras2[0][0] + palabras2[1][0] + palabras2[2][0])
+
+print("Coding For All".index("C"))
+print("Coding For All".index("F"))
+print("Coding For All People".rfind("l"))
+
+print("You cannot end a sentence with because because because is a conjunction".find("because"))
+print("You cannot end a sentence with because because because is a conjunction".rfind("because"))
+print("You cannot end a sentence with because because because is a conjunction".replace("because because because",''))
+print("You cannot end a sentence with because because because is a conjunction".find("because"))
+print("You cannot end a sentence with because because because is a conjunction".replace("because because because",''))
