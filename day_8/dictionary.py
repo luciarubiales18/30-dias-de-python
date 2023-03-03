7756538604c416479f57846d9f3ab7cb555587e0
LUCIA <cbechur0606@m.educaand.es>
2023-03-03 11:11:29 +0100

    cambio de todos los dias

--- day_8/dictionary.py
+++ day_8/dictionary.py
@@ -1,5 +1,43 @@
 dog = {}
-dog.append["Triana","Blanco", "Bichon Maltes", "cuatro patas", "5 years" ]
+dog = {
+    "name" : "Triana",
+    "color" : "Blanca",
+    "breed" : "Bichonmaltes",
+    "legs" : "Cuatro patas",
+    "age" : "5 años"}
+    
 print(dog)
 
-student
+student = {
+    "first_name": "Lucia",
+    "last_name" : "Rubiales",
+    "gender" : "Mujer",
+    "age" : "16",
+    "marital status" : "Con novio",
+    "skills" : ["Dibujar", "Estudiar"],
+    "country" : "España",
+    "city" : "Jerez",
+    "addres" : "Barriada San Jose Obrero", }
+print(student)
+print(len(student))
+print(student["skills"])
+
+print(type(student))
+
+student["skills"].append("Bailar") 
+student["skills"].append("Cantar")
+print(student)
+
+keys = student.keys()
+print(keys)
+
+values = student.values()
+print(student)
+
+print(student.items())
+
+print(student.clear())
+
+del student
+
+
