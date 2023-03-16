6870c286d04b7cd581cb56f61dc49762e63bfa7a
LUCIA <cbechur0606@m.educaand.es>
2023-03-16 09:17:59 +0100

    Todas las modificaciones

---
+++ day_9/conditional.py
@@ -0,0 +1,34 @@
+age = int(input("Enter your age :"))
+if age > 18 :
+    print("You are old enough to learn to drive")
+
+elif age < 18 :
+    years = 18 - age
+    print(f"Missing {years} age for 18")
+
+
+my_age = input ("16")
+your_age = input("Enter your age : ")
+if my_age < your_age :
+    print("You are older than me")
+
+elif my_age > your_age :
+    print("you are most mayor")
+
+a = input("Enter one number : ")
+b = input ("20")
+
+if a > b :
+    print("a is greater than b")
+if a < b :
+    print("a is smaller than b")
+
+
+season = {
+    "Autumn": "September, October, November" ,
+    "Winter": "December, January, February" , 
+    "Spring": "March, April, May",
+    "Summer": "June, July, August" , }
+
+
+
